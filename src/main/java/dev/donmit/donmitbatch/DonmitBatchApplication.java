3a2995cbc2e03b064fb11a42a8423d3a04fbe176
0n1dev <all_mind@naver.com>
2023-02-25 00:45:17 +0800

    first commit

---
+++ src/main/java/dev/donmit/donmitbatch/DonmitBatchApplication.java
@@ -0,0 +1,13 @@
+package dev.donmit.donmitbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonmitBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonmitBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/donmit/donmitbatch/DonmitBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.donmit.donmitbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonmitBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
